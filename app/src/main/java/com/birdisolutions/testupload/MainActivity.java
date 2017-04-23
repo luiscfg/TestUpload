548e9c67cc7461ede518ed63a9dc4597405d2c93
Luis Carlos FERNÁNDEZ <luiscarlosfernandez@MacBook-Air-de-Luis.local>
2017-04-23 14:06:05 +0200

    Initial commit

---
+++ app/src/main/java/com/birdisolutions/testupload/MainActivity.java
@@ -0,0 +1,20 @@
+package com.birdisolutions.testupload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void TeestUpload(){
+
+        //Estos es una prueba
+        int i= 0;
+        i= ++i;
+
+    }
+}
